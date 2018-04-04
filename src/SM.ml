5498ff788ea9707a66fbe05424934cca671556ee
Dmitry Mukhutdinov <flyingleafe@gmail.com>
2018-04-05 00:10:58 +0300

    [HW6] Support procedures in stack machine interpreter

--- src/SM.ml
+++ src/SM.ml
@@ -33,9 +33,15 @@ type config = (prg * State.t) list * int list * Stmt.config
    environment is used to locate a label to jump to (via method env#labeled <label_name>)
  *)
 let check_cond x = function
-  | "nz"  -> x <> 0
-  | "z" -> x = 0
-  | _       -> failwith "Invalid condition type"
+  | "nz" -> x <> 0
+  | "z"  -> x = 0
+  | _    -> failwith "Invalid condition type"
+
+let rec split_list n l =
+  if n = 0 then [], l else
+    (match l with
+     | []    -> failwith ("List length mismatch: list length is less than " ^ string_of_int n)
+     | x::xs -> let ys, zs = split_list (n - 1) xs in x::ys, zs)
 
 let rec eval env ((cstack, stack, ((sf, input, output) as in_env)) as config) = function
   | [] -> config
@@ -51,6 +57,16 @@ let rec eval env ((cstack, stack, ((sf, input, output) as in_env)) as config) = 
      | CJMP (cond, label) ->
        eval env (cstack, tl stack, in_env)
          (if check_cond (hd stack) cond then (env#labeled label) else ops)
+     | BEGIN (args, locals) ->
+       let arg_vals, stack' = split_list (length args) stack in
+       let sf' = State.enter sf (args @ locals) in
+       let sf' = fold_left (fun s (p, v) -> State.update p v s) sf' (combine args arg_vals) in
+       eval env (cstack, stack', (sf', input, output)) ops
+     | END -> (match cstack with
+         | (prg, sf') :: cstack' -> eval env (cstack', stack, (State.leave sf sf', input, output)) prg
+         | []                    -> config)
+     | CALL func ->
+       eval env ((ops, sf) :: cstack, stack, in_env) (env#labeled func)
      | BINOP op  ->
        let y :: x :: rest = stack
        in eval env (cstack, Expr.eval_op op x y :: rest, (sf, input, output)) ops
@@ -91,7 +107,7 @@ let rec compile_expr e =
   | Expr.Var x -> [LD x]
   | Expr.Binop (op, a, b) -> compile_expr a @ compile_expr b @ [BINOP op]
 
-let rec compile' lbls st =
+let rec compile_stmt lbls st =
   match st with
   | Stmt.Read x        -> lbls, [READ; ST x]
   | Stmt.Write e       -> lbls, compile_expr e @ [WRITE]
@@ -101,8 +117,8 @@ let rec compile' lbls st =
     let lbl_els, lbls' = lbls#new_label in
     let lbl_end, lbls1 = lbls'#new_label in
     let cond_code = compile_expr cond in
-    let lbls2, the_code = compile' lbls1 the in
-    let lbls3, els_code = compile' lbls2 els in
+    let lbls2, the_code = compile_stmt lbls1 the in
+    let lbls3, els_code = compile_stmt lbls2 els in
     let end_jump, end_label =
       (match els_code with [] -> [], [] | _ -> [JMP lbl_end], [LABEL lbl_end]) in
     lbls3,
@@ -112,19 +128,35 @@ let rec compile' lbls st =
     let lbl_begin, lbls' = lbls#new_label in
     let lbl_end, lbls1 = lbls'#new_label in
     let cond_code = compile_expr cond in
-    let lbls2, act_code = compile' lbls1 action in
+    let lbls2, act_code = compile_stmt lbls1 action in
     lbls2,
     [LABEL lbl_begin] @ cond_code @ [CJMP ("z", lbl_end)] @
     act_code @ [JMP lbl_begin] @ [LABEL lbl_end]
   | Stmt.Repeat (action, cond) ->
     let lbl_begin, lbls' = lbls#new_label in
-    let lbls1, act_code = compile' lbls' action in
+    let lbls1, act_code = compile_stmt lbls' action in
     let cond_code = compile_expr cond in
     lbls1,
     [LABEL lbl_begin] @ act_code @ cond_code @ [CJMP ("z", lbl_begin)]
+  | Stmt.Call (func, args) ->
+    let args_prepare = concat (rev_map compile_expr args) in
+    lbls, args_prepare @ [CALL func]
   | Stmt.Seq (s, s') ->
-    let lbls1, fst = compile' lbls s in
-    let lbls2, snd = compile' lbls1 s' in
+    let lbls1, fst = compile_stmt lbls s in
+    let lbls2, snd = compile_stmt lbls1 s' in
     lbls2, fst @ snd
 
-let compile (defs, st) = let _, prog = compile' (new labels) st in prog
+let compile_def lbls (func, (params, locals, body)) =
+  let lbls', cbody = compile_stmt lbls body in
+  lbls', [LABEL func; BEGIN (params, locals)] @ cbody @ [END]
+
+let compile (defs, st) =
+  let lbls = new labels in
+  let lbls, main = compile_stmt lbls st in
+  let compile_and_combine (lbls', prev) def =
+    let lbls', prg = compile_def lbls' def in
+    lbls', prev @ prg
+  in
+  let _, funcs = fold_left compile_and_combine (lbls, []) defs in
+  [LABEL "main"] @ main @ [END] @ funcs
+
